002d5dcbbd9edb6f3b19e3e1c1ab2710e93d12bc
Orion-Artemis <omlande251220036@gmail.com>
2024-03-20 00:43:01 +0530

    Made a Sign up page and integrated with Firebase email and password ||next task Log in page and home page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,8 @@
+import com.android.build.api.dsl.BuildFeatures
+
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -29,6 +32,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures{
+        viewBinding;
+    }
 }
 
 dependencies {
@@ -36,7 +42,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.code.gson:gson:2.10")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.google.firebase:firebase-auth")
+    implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
 }
--- app/src/main/java/com/example/mad/MainActivity2.java
+++ app/src/main/java/com/example/mad/MainActivity2.java
@@ -3,12 +3,80 @@ package com.example.mad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity2 extends AppCompatActivity {
+    private Button signUp;
+    private EditText name,email,phone,pass;
+    private TextView loginRedirect;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        name = findViewById(R.id.nameIn);
+        email = findViewById(R.id.emailIn);
+        phone = findViewById(R.id.phoneIn);
+        pass = findViewById(R.id.passIn);
+        signUp = findViewById(R.id.signUpBt);
+        loginRedirect = findViewById(R.id.loginRedirect);
+        mAuth = FirebaseAuth.getInstance();
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = name.getText().toString().trim();
+                String cred = pass.getText().toString().trim();
+                String num = phone.getText().toString().trim();
+                String mail = email.getText().toString().trim();
+
+                if (user.isEmpty() || cred.isEmpty() || num.isEmpty() || mail.isEmpty()){
+                    Toast.makeText(MainActivity2.this,"Please Enter all Credentials",Toast.LENGTH_SHORT).show();
+                } else {
+                    mAuth.signInWithEmailAndPassword(mail , cred).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(MainActivity2.this, "SignUp Successful", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(MainActivity2.this, "SignUp Failed Try Again", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+        loginRedirect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            startActivity(new Intent(MainActivity2.this, MainActivity.class));
+            finish();
+        }
     }
 }
